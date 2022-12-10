ff72dcdabdc3e708a20eacf7b30313ee0afda2df
nakul412 <nakul1636.be20@chitkara.edu.in>
2022-12-10 23:52:57 +0530

    changed css of register page

--- front/src/components/Register.jsx
+++ front/src/components/Register.jsx
@@ -33,33 +33,33 @@ const Register = () => {
     return (
         <div className="container-fluid vh-100 w-100 d-flex justify-content-center align-items-center bgset">
              
-        <div className="container bg-light w-50 h-75 borderRadius bg-dark text-white">
+        <div className="container  w-50 h-75 borderRadius text-black ">
          <div className="mx-5 text-center">
           
         <h2>Register</h2>
 </div>
         <form className="form mt-3 w-75 mx-auto" onSubmit={handleSubmit} >
         <div className="form-group mb-3">
-            <label>Username</label>
-            <input type="text" placeholder="Type your username" className="form-control input no-outline" name="username" onChange={changevalue} value={formData.username} required/>
+            <label className='dark'>Username</label>
+            <input type="text" placeholder="Type your username" className="form-control input" name="username" onChange={changevalue} value={formData.username} required/>
         </div>
         <div className="form-group mb-3">
-            <label>Email</label>
-            <input type="email" placeholder="Type your username" className="form-control input no-outline" name="email" onChange={changevalue} value={formData.email} required/>
+            <label className='dark '>Email</label>
+            <input type="email" placeholder="Type your username" className="form-control input " name="email" onChange={changevalue} value={formData.email} required/>
         </div>
         <div className="form-group mb-3">
-            <label>Password</label>
-            <input type="password" placeholder="Type your password" className="form-control input no-outline" name="password" onChange={changevalue} value={formData.password} required/>
+            <label className='dark'>Password</label>
+            <input type="password" placeholder="Type your password" className="form-control input " name="password" onChange={changevalue} value={formData.password} required/>
         </div>
 
         <div className="form-group mb-3">
-        <label>Confirm Password</label>
-        <input type="password" placeholder="Type your password" className="form-control input no-outline" name="confirmpass" onChange={changevalue} value={formData.confirmpass} required/>
+        <label className='dark'>Confirm Password</label>
+        <input type="password" placeholder="Type your password" className="form-control input" name="confirmpass" onChange={changevalue} value={formData.confirmpass} required/>
     </div>
 
     <div className="form-group mb-3">
-    <label>Phone No.</label>
-    <input type="text" placeholder="Type your contact no." className="form-control input no-outline" name="phone" onChange={changevalue} value={formData.phone} required/>
+    <label className='dark'>Phone No.</label>
+    <input type="text" placeholder="Type your contact no." className="form-control input " name="phone" onChange={changevalue} value={formData.phone} required/>
 </div>
 <div className="text-center">
    <button className="btn btn-group text-light"  type='submit' >Submit</button>
