3356c218bb1e2b597f31017535505a6bf0df9657
harshkumar <harsh1559.be20@gmail.com>
2023-01-11 15:22:57 +0530

    home page styling

--- front/src/components/UpperHalf.jsx
+++ front/src/components/UpperHalf.jsx
@@ -20,7 +20,7 @@ const UpperHalf = (props) => {
         <div style={{display:"flex"}} className='pt-3 container-fluid'>
         
         <div className='w-50' style={{cursor:"pointer",marginLeft:"4%"}} ><NavLink to="/"> <font face="Comic sans MS"  size='5' color="black" style={{ textDecoration: "underline" }}>Recipe
-                </font><img src='https://cdn-icons-png.flaticon.com/512/1721/1721455.png' style={{width:"50px"}} /></NavLink></div>
+                </font><img src='https://cdn-icons-png.flaticon.com/512/1721/1721455.png' style={{width:"43px"}} /></NavLink></div>
                 <div className=' w-50  text-center' style={{marginLeft:"25%"}}>
  <h3 style={{textTransform:"capitalize"}}>{name}<UilUser></UilUser></h3>
             </div>
